c753d2f20be37064ca111824221d374a43247673
Anmol Mehla <anmolmehla1@outlook.com>
2025-06-30 08:46:29 +0530

    DOM_Manuplation

---
+++ Objects.js
@@ -0,0 +1,15 @@
+const Person = {
+    name : "Anmol",
+    age : 18,
+    isActive : false
+};
+
+Person.name = "Akshit";
+Person.fName = "Rajpal";
+
+delete Person.isActive;
+const Persons = new Object()
+
+Persons.name = "Anmol"
+
+console.log(Person);
